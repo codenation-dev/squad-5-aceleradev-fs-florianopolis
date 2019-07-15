abee699a9ba8659ddaa21ec8d4d8cda38fe990ba
Mark <marcelokopmann@gmail.com>
2019-07-14 21:45:59 -0300

    New page added: Notifications; SideBar converted into Drawer in the new Layout component

--- frontend/uatireact/src/App.js
+++ frontend/uatireact/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 // prettier-ignore
-import { SideBar, Login, Register, ImportPage, Dashboard, Admin } from './components'
+import { Login, Register, ImportPage, Dashboard, Admin } from './components'
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { Grid } from "@material-ui/core/";
+import Layout from "./components/Layout";
 
 const App = props => {
   const HomeRoute = () => {
@@ -16,22 +16,16 @@ const App = props => {
 
   return (
     <div className="App">
-      <SideBar />
-      <Grid container>
-        <Grid item xs={3}>
-          <h1>Hello</h1>
-        </Grid>
-        <Grid item xs={9}>
-          <Switch>
-            <Route exact path="/" component={HomeRoute} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/import" component={ImportPage} />
-            <Route path="/admin" component={Admin} />
-          </Switch>
-        </Grid>
-      </Grid>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={HomeRoute} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/import" component={ImportPage} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
+      </Layout>
     </div>
   );
 };
--- frontend/uatireact/src/components/index.js
+++ frontend/uatireact/src/components/index.js
@@ -3,6 +3,5 @@ import Dashboard from "./Dashboard";
 import ImportPage from "./ImportPage";
 import Login from "./Login";
 import Register from "./Register";
-import SideBar from "./SideBar";
 
-export { Admin, Dashboard, ImportPage, Login, Register, SideBar };
+export { Admin, Dashboard, ImportPage, Login, Register };
