87a267330f1a6cc25e504d885ab96f0ee0076de2
Gabriel Tomazi <gabriel_tomazi@hotmail.com>
2019-07-16 15:30:45 -0300

    Ajuste de estrutura local

--- frontend/uatireact/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Admin from "./Admin";
-import Dashboard from "./Dashboard";
-import ImportPage from "./ImportPage";
-import Login from "./Login";
-import Register from "./Register";
-import Notifications from "./Notifications";
-
-export { Admin, Dashboard, ImportPage, Login, Register, Notifications };
