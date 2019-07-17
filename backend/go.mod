2743fd0ca77405a447e669d1249c8dd877b29c8f
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2019-07-16 21:45:44 -0300

    fixed main

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.7.3
 	github.com/lib/pq v1.1.1
+	github.com/stretchr/testify v1.3.0
 	github.com/subosito/gotenv v1.1.1
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
 )
---
+++ backend/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"uati-api/clients"
+	"uati-api/database"
+	"uati-api/middlewares"
+	"uati-api/users"
+
+	"github.com/gorilla/mux"
+
+	"github.com/subosito/gotenv"
+)
+
+//Db is the connection to the db
+var db = database.GetDB()
+
+func init() {
+	gotenv.Load()
+}
+
+func main() {
+	setdb := flag.Bool("setdb", false, "download csv and setup db on startup")
+
+	flag.Parse()
+
+	if *setdb {
+		database.SetDB()
+		fmt.Println("DB set, starting server")
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/login", users.Login).Methods("POST")
+	router.HandleFunc("/", home).Methods("get")
+
+	router.HandleFunc("/signup", middlewares.TokenVerifyMiddleware(users.Signup)).Methods("POST")
+	router.HandleFunc("/clients", middlewares.TokenVerifyMiddleware(clients.GetClients)).Methods("GET")
+	router.HandleFunc("/updateSalary", middlewares.TokenVerifyMiddleware(clients.UpdateSalary)).Methods("PUT")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("DB running at port 5432\nServer running at port 8080"))
+}
