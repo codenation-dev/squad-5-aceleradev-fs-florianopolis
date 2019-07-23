e0c0cb02c1b538cf2d7dad02d8e18d19a316ec8b
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2019-07-23 19:40:55 -0300

    fixed port

--- backend/users/users.go
+++ backend/users/users.go
@@ -73,7 +73,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	password := user.Password
 
 	row := db.QueryRow("SELECT * FROM users where email=$1", user.Email)
-	err := row.Scan(&user.ID, &user.Email, &user.Password)
+	err := row.Scan(&user.ID, &user.Email, &user.Name, &user.Password)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			error.Message = "The user does not exists"
