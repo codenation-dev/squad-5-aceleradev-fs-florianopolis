e303d192a56c33615d4eb56bd9b0bd5f6a7cf7c0
gatomazi <44870871+gatomazi@users.noreply.github.com>
2019-07-27 13:02:20 -0300

    Atualizando branch

--- frontend/uatireact/src/pages/Notifications/index.js
+++ frontend/uatireact/src/pages/Notifications/index.js
@@ -9,6 +9,7 @@ import { ExpansionPanelStyled, ExpansionPanelDetailsStyled, FakeExpand, UlStyled
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class Notifications extends Component {
   constructor (){
@@ -23,7 +24,6 @@ class Notifications extends Component {
   }
 
   handleClick(number) {
-    console.log(number);
     this.paginationFunct(number, this.props.notificationList);
   }
   
@@ -45,8 +45,7 @@ class Notifications extends Component {
     const indexOfLastTodo = currentPage * alertsPerPage;
     const indexOfFirstTodo = indexOfLastTodo - alertsPerPage;
     const notificationList = data.slice(indexOfFirstTodo, indexOfLastTodo);
-    console.log(notificationList);
-    this.setState({notificationList, totalLength: data.length});
+    this.setState({notificationList, currentPage, totalLength: data.length});
   }
 
   mountPageBlocks(){
@@ -59,6 +58,7 @@ class Notifications extends Component {
         <li
           key={number}
           onClick={() => this.handleClick(number)}
+          style={{backgroundColor: this.state.currentPage === number ? `#DDD` : ""}}
         >
           {number}
         </li>
@@ -76,53 +76,60 @@ class Notifications extends Component {
     return (
       <div style={{ padding: "15px" }}>
         <h1>Notificações</h1>
-        {this.state.notificationList.map((item, index) => {
-          let splitName = item.name.split(",");
-          let data = splitName.map(name => {
-            return {
-              name: name
-            };
-          });
-          return (
-            <React.Fragment key={index}>
-              {item.isClientEmail &&
-                <FakeExpand>
-                  <Typography>O cliente {data[0].name} se tornou um funcionário publico</Typography>
-                </FakeExpand>
-              }
-
-              {!item.isClientEmail &&
-              <ExpansionPanelStyled>
-                <ExpansionPanelSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography>
-                    Existem {splitName.length} clientes em potencial na atualização de {item.sentAt}
-                  </Typography>
-                </ExpansionPanelSummary>
-                  <ExpansionPanelDetailsStyled>
-                    <div style={{ width: "100%" }}>
-                      <EnhancedTable dataAtualizacao={item.sentAt} dados={data} />
-                    </div>
-                  </ExpansionPanelDetailsStyled>
-                </ExpansionPanelStyled>
-              }
-              
-            </React.Fragment>
-          );
-        })}
-        <UlStyled id="page-numbers">
-            {this.mountPageBlocks()}
-        </UlStyled>
+        {this.props.loading ? (
+          <div style={{padding: "25vh"}}>
+            <CircularProgress />
+          </div>
+        ) : (
+        <React.Fragment>
+          {this.state.notificationList.map((item, index) => {
+            let splitName = item.name.split(",");
+            let data = splitName.map(name => {
+              return {
+                name: name
+              };
+            });
+            return (
+              <React.Fragment key={index}>
+                {item.isClientEmail ? (
+                  <FakeExpand>
+                    <Typography>O cliente {data[0].name} se tornou um funcionário publico</Typography>
+                  </FakeExpand>
+                ) : (
+                <ExpansionPanelStyled>
+                  <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography>
+                      Existem {splitName.length} clientes em potencial na atualização de {item.sentAt}
+                    </Typography>
+                  </ExpansionPanelSummary>
+                    <ExpansionPanelDetailsStyled>
+                      <div style={{ width: "100%" }}>
+                        <EnhancedTable dataAtualizacao={item.sentAt} dados={data} />
+                      </div>
+                    </ExpansionPanelDetailsStyled>
+                  </ExpansionPanelStyled>
+                )}
+                
+              </React.Fragment>
+            );
+          })}
+          <UlStyled id="page-numbers">
+              {this.mountPageBlocks()}
+          </UlStyled>
+        </React.Fragment>
+        )}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  notificationList: state.notificationReducer.notificationList
+  notificationList: state.notificationReducer.notificationList,
+  loading: state.notificationReducer.loading
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(allActions, dispatch);
