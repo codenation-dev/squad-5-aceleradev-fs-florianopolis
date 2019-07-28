f2c381f2fd82ec3012c749e78f4950ec0914d8fb
Mark Kop <mk.marcelo.mk@gmail.com>
2019-07-28 19:01:39 -0300

    added sentTo and sentAt in notifications page; removed mockup account

--- frontend/uatireact/src/pages/Notifications/index.js
+++ frontend/uatireact/src/pages/Notifications/index.js
@@ -5,28 +5,33 @@ import * as allActions from "../../redux/actions";
 
 import { EnhancedTable } from "../../components/NotificationTable";
 
-import { ExpansionPanelStyled, ExpansionPanelDetailsStyled, FakeExpand, UlStyled } from "./styles";
+import {
+  ExpansionPanelStyled,
+  ExpansionPanelDetailsStyled,
+  UlStyled
+} from "./styles";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Paper from "@material-ui/core/Paper";
 
 class Notifications extends Component {
-  constructor (){
+  constructor() {
     super();
-    this.state ={
+    this.state = {
       currentPage: 1,
       alertsPerPage: 10,
       notificationList: [],
       totalLength: 0
-    }
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(number) {
     this.paginationFunct(number, this.props.notificationList);
   }
-  
+
   componentDidMount() {
     const asyncFunc = async () => {
       await this.props.getNotifications();
@@ -39,17 +44,21 @@ class Notifications extends Component {
     this.paginationFunct(1, nextProps.notificationList);
   }
 
-  paginationFunct(currentPage, data = []){
+  paginationFunct(currentPage, data = []) {
     const { alertsPerPage } = this.state;
     const indexOfLastTodo = currentPage * alertsPerPage;
     const indexOfFirstTodo = indexOfLastTodo - alertsPerPage;
     const notificationList = data.slice(indexOfFirstTodo, indexOfLastTodo);
-    this.setState({notificationList, currentPage, totalLength: data.length});
+    this.setState({ notificationList, currentPage, totalLength: data.length });
   }
 
-  mountPageBlocks(){
+  mountPageBlocks() {
     const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(this.state.totalLength / this.state.alertsPerPage); i++) {
+    for (
+      let i = 1;
+      i <= Math.ceil(this.state.totalLength / this.state.alertsPerPage);
+      i++
+    ) {
       pageNumbers.push(i);
     }
     const renderPageNumbers = pageNumbers.map(number => {
@@ -57,7 +66,9 @@ class Notifications extends Component {
         <li
           key={number}
           onClick={() => this.handleClick(number)}
-          style={{backgroundColor: this.state.currentPage === number ? `#DDD` : ""}}
+          style={{
+            backgroundColor: this.state.currentPage === number ? `#DDD` : ""
+          }}
         >
           {number}
         </li>
@@ -70,56 +81,66 @@ class Notifications extends Component {
   render() {
     // let names = this.props.notificationList.name.split(",");
     // console.log(names);
-    
 
     return (
       <div style={{ padding: "15px" }}>
         <h1>Notificações</h1>
         {this.props.loading ? (
-          <div style={{padding: "25vh"}}>
+          <div style={{ padding: "25vh" }}>
             <CircularProgress />
           </div>
         ) : (
-        <React.Fragment>
-          {this.state.notificationList.map((item, index) => {
-            let splitName = item.name.split(",");
-            let data = splitName.map(name => {
-              return {
-                name: name
-              };
-            });
-            return (
-              <React.Fragment key={index}>
-                {item.isClientEmail ? (
-                  <FakeExpand>
-                    <Typography>O cliente {data[0].name} se tornou um funcionário publico</Typography>
-                  </FakeExpand>
-                ) : (
-                <ExpansionPanelStyled>
-                  <ExpansionPanelSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography>
-                      Existem {splitName.length} clientes em potencial na atualização de {item.sentAt}
-                    </Typography>
-                  </ExpansionPanelSummary>
-                    <ExpansionPanelDetailsStyled>
-                      <div style={{ width: "100%" }}>
-                        <EnhancedTable dataAtualizacao={item.sentAt} dados={data} />
-                      </div>
-                    </ExpansionPanelDetailsStyled>
-                  </ExpansionPanelStyled>
-                )}
-                
-              </React.Fragment>
-            );
-          })}
-          <UlStyled id="page-numbers">
-              {this.mountPageBlocks()}
-          </UlStyled>
-        </React.Fragment>
+          <React.Fragment>
+            {this.state.notificationList.map((item, index) => {
+              let splitName = item.name.split(",");
+              let data = splitName.map(name => {
+                return {
+                  name: name
+                };
+              });
+              return (
+                <React.Fragment key={index}>
+                  {item.isClientEmail ? (
+                    <>
+                      <Paper elevation="5">
+                        <Typography component="h4">
+                          O cliente {data[0].name} se tornou um funcionário
+                          publico
+                        </Typography>
+                        <Typography component="p">
+                          Notificação enviada para {item.sentTo} em{" "}
+                          {item.sentAt}
+                        </Typography>
+                      </Paper>
+                      <br />
+                    </>
+                  ) : (
+                    <ExpansionPanelStyled>
+                      <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                      >
+                        <Typography>
+                          Existem {splitName.length} clientes em potencial na
+                          atualização de {item.sentAt}
+                        </Typography>
+                      </ExpansionPanelSummary>
+                      <ExpansionPanelDetailsStyled>
+                        <div style={{ width: "100%" }}>
+                          <EnhancedTable
+                            dataAtualizacao={item.sentAt}
+                            dados={data}
+                          />
+                        </div>
+                      </ExpansionPanelDetailsStyled>
+                    </ExpansionPanelStyled>
+                  )}
+                </React.Fragment>
+              );
+            })}
+            <UlStyled id="page-numbers">{this.mountPageBlocks()}</UlStyled>
+          </React.Fragment>
         )}
       </div>
     );
--- frontend/uatireact/src/services/login.js
+++ frontend/uatireact/src/services/login.js
@@ -2,20 +2,7 @@ import { post } from "../utils/api";
 
 class ServiceLogin {
   static tryLogin(payload) {
-    console.log(payload);
-    const isBackendRunning = true; //change this to test the code without bacakend's endpoints
-    if (isBackendRunning) {
-      return post("login", payload);
-    } else {
-      if (
-        payload.credentials.username === "1" &&
-        payload.credentials.password === "1"
-      ) {
-        return { token: "fakeToken" };
-      } else {
-        return { message: "fakeLogin failed" };
-      }
-    }
+    return post("login", payload);
   }
 }
 
